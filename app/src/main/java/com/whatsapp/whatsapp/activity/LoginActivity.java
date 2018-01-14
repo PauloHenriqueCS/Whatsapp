22eb8a0a61d443265aff6b5a7794056557692fd4
PAULO\paulo <paulo.hc@outlook.com>
2018-01-14 14:21:38 -0200

    Login

--- app/src/main/java/com/whatsapp/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/whatsapp/whatsapp/activity/LoginActivity.java
@@ -15,27 +15,56 @@ import android.widget.EditText;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.whatsapp.whatsapp.R;
 import com.whatsapp.whatsapp.config.ConfiguracaoFirebase;
 import com.whatsapp.whatsapp.helper.Permissao;
 import com.whatsapp.whatsapp.helper.Preferencias;
+import com.whatsapp.whatsapp.model.Usuario;
 
 
 import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private
-
-
+    private TextView email;
+    private TextView senha;
+    private Button buttonLogin;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (TextView) findViewById(R.id.email);
+        senha = (TextView) findViewById(R.id.senha);
+        buttonLogin = (Button) findViewById(R.id.login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                usuario = new Usuario();
+                usuario.setEmail( email.getText().toString());
+                usuario.setSenha( email.getText().toString());
+                validaLogin();
+            }
+        });
+
+ 
+    }
+
+    public void validaLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword()
+
     }
 
     public void abriCadastroUsuario(View view){
